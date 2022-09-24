f5060bf4c83b50632b6578503992c862fdf21ffe
kaczmprz <kaczmprz@gmail.com>
2022-09-24 13:32:08 +0200

    Changes in terraform s3 bucket

--- terraform/s3/bucket.tf
+++ terraform/s3/bucket.tf
@@ -1,4 +1,13 @@
-resource "aws_s3_bucket" "staging" {
-    bucket = "${var.bucket_name}"
-    acl = "${var.acl_value}"
+resource "aws_s3_bucket" "staging_bucket" {
+  bucket = "mdw-staging"
+
+  tags = {
+    Name = "stage"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "staging_bucket_acl" {
+  bucket = aws_s3_bucket.staging_bucket.id
+  acl    = "private"
 }
