bf95b07daa460ad47db7a50c3c11afdac6e0a30d
kaczmprz <kaczmprz@gmail.com>
2022-11-05 17:31:49 +0100

    Changes in pipeplines, added params

--- pipelines/pipelines.py
+++ pipelines/pipelines.py
@@ -2,11 +2,15 @@ from prefect import task, flow
 from prefect.filesystems import S3
 from prefect_snowflake.database import SnowflakeConnector, SnowflakeCredentials
 from prefect_snowflake.database import snowflake_query, snowflake_multiquery
+from datetime import datetime
 
 s3_block = S3.load("s3-retail")
 snowflake_connector = SnowflakeConnector.load("snowflake-connector-retail")
 
-def etl_load_stage(table: str, columns: list, stage: str, path: str):
+now = datetime.now()
+year, month, day = now.year, now.month, now.day
+
+def etl_load_stage(table: str, columns: list, stage: str, path: str, params: dict):
     columns_ = ", ".join(columns)
     stage_columns = ["replace($" + str(i+1) + ",'\"','')" for i in range(len(columns))]
     stage_columns_ = ", ".join(stage_columns)
@@ -24,9 +28,7 @@ def etl_load_stage(table: str, columns: list, stage: str, path: str):
     result_load = snowflake_query(
         query = load_stage_table,
         snowflake_connector = snowflake_connector,
-        params={"day": 5,
-                "month": 11,
-                "year": 2022},
+        params=params
     )
     return result_load, result_truncate
 
@@ -41,17 +43,26 @@ def etl_load_prod(table: str):
 
 @flow
 def etl_load_dim_customer_flow():
-    etl_load_stage('customer', ['name', 'birth_date', 'town', 'email'], 'mdw_staging', 'customers')
+    params = {"day": day,
+              "month": month,
+              "year": year}
+    etl_load_stage('customer', ['name', 'birth_date', 'town', 'email'], 'mdw_staging', 'customers', params)
     etl_load_prod('customer')
 
 @flow
 def etl_load_dim_product_flow():
-    etl_load_stage('product', ['ean', 'category', 'net_price'], 'mdw_staging', 'products')
+    params = {"day": day,
+              "month": month,
+              "year": year}
+    etl_load_stage('product', ['ean', 'category', 'net_price'], 'mdw_staging', 'products', params)
     etl_load_prod('product')
 
 @flow
 def etl_load_dim_shop_flow():
-    etl_load_stage('shops', ['id', 'city'], 'mdw_staging', 'shops')
+    params = {"day": day,
+              "month": month,
+              "year": year}
+    etl_load_stage('shops', ['id', 'city'], 'mdw_staging', 'shops', params)
     etl_load_prod('shops')
 
 
